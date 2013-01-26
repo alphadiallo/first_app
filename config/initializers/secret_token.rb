d171ca30c8323af3c4fac74bbf5bb865c0f68a8f
alpha <alpha.oumou@yahoo.fr>
2013-01-26 20:03:44 +0100

    changement de l'initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FistApp::Application.config.secret_token = '8112370092a4d8ec0e179466ad92905387edc26ef3ba6cd1dba220c3e19d4aebfe5bf3ccbad8464016ebf3a471ad985df3d7c20fff731d2601418ac63f154f23'
